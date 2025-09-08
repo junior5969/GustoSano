7aec61175d075d16ce0d5a897fd66818953682b8
Chiara <clodiauni@gmail.com>
2025-09-08 14:38:09 +0200

    fix firebase deploy

--- functions/index.ts
+++ functions/index.ts
@@ -2,28 +2,25 @@ import * as functions from "firebase-functions";
 import fetch from "node-fetch";
 
 export const getFruits = functions.https.onRequest(async (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET");
+
   try {
-    const response = await fetch("https://www.fruityvice.com/api/fruit/all");
+    const path = req.path; // es: /fruit/all oppure /fruit/apple
+    let apiUrl = "https://www.fruityvice.com/api";
+
+    if (path === "/fruit/all") {
+      apiUrl += "/fruit/all";
+    } else if (path.startsWith("/fruit/")) {
+      const name = path.split("/").pop();
+      apiUrl += `/fruit/${name}`;
+    } else {
+      res.status(404).send("Endpoint non trovato");
+      return;
+    }
+
+    const response = await fetch(apiUrl);
     const data = await response.json();
-
-    // Permette richieste dal client
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Methods", "GET");
-    res.json(data);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-});
-
-// Per frutto singolo
-export const getSingleFruit = functions.https.onRequest(async (req, res) => {
-  const name = req.path.split('/').pop();
-  try {
-    const response = await fetch(`https://www.fruityvice.com/api/fruit/${name}`);
-    const data = await response.json();
-
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Methods", "GET");
     res.json(data);
   } catch (err) {
     res.status(500).send(err.message);
